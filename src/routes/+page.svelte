218d13f915573f94f93fce0f44ba96ab9e3a188a
gg <>
2023-04-02 19:04:44 +0300

    Update: Basic UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,93 @@
-<style lang="postcss"></style>
+<body class="bg-gradient-to-r from-cyan-500 to-blue-500">
+	<div class="bg-blue-50 flex justify-left p-2 border-4 rounded-b-lg border-blue-50 border-t-0">
+		<h1
+			class="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500"
+		>
+			Hydro homies
+		</h1>
+	</div>
+	<br />
+	<div class="border-4 mx-auto rounded-lg border-blue-50 max-w-xl bg-blue-50 p-2">
+		<h2
+			class="flex justify-center p-2 text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500"
+		>
+			Степен на дехидратация
+		</h2>
+		<ul class="p-2 space-y-2">
+			<li>
+				<div class="bg-gradient-to-r from-cyan-500 to-blue-500 pl-4 rounded-lg p-1">
+					<input type="radio" id="niska" name="stepen" class="hidden peer" required />
+					<label
+						for="niska"
+						class="inline-flex w-full p-2 text-blue-500 bg-blue-50 rounded-md cursor-pointer peer-checked:hidden"
+					>
+						<div class="block">
+							<div
+								class="w-full text-lg font-semibold text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500 peer-checked/radio:bg-blue-50"
+							>
+								Лека
+							</div>
+						</div>
+					</label>
+					<div class="hidden peer-checked:block">
+						<label
+							for="niska"
+							class="inline-flex w-full p-2 bg-blue-50 rounded-md cursor-pointer text-lg font-semibold text-transparent bg-clip-text"
+							>Лека
+						</label>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="bg-gradient-to-r from-cyan-500 to-blue-500 pl-4 rounded-lg p-1">
+					<input type="radio" id="sredna" name="stepen" class="hidden peer" required />
+					<label
+						for="sredna"
+						class="inline-flex w-full p-2 text-blue-500 bg-blue-50 rounded-md cursor-pointer peer-checked:hidden"
+					>
+						<div class="block">
+							<div
+								class="w-full text-lg font-semibold text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500 peer-checked/radio:bg-blue-50"
+							>
+								Средна
+							</div>
+						</div>
+					</label>
+					<div class="hidden peer-checked:block">
+						<label
+							for="sredna"
+							class="inline-flex w-full p-2 bg-blue-50 rounded-md cursor-pointer text-lg font-semibold text-transparent bg-clip-text"
+							>Средна
+						</label>
+					</div>
+				</div>
+			</li>
+			<li>
+				<div class="bg-gradient-to-r from-cyan-500 to-blue-500 pl-4 rounded-lg p-1">
+					<input type="radio" id="visoka" name="stepen" class="hidden peer" required />
+					<label
+						for="visoka"
+						class="inline-flex w-full p-2 text-blue-500 bg-blue-50 rounded-md cursor-pointer peer-checked:hidden"
+					>
+						<div class="block">
+							<div
+								class="w-full text-lg font-semibold text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500 peer-checked/radio:bg-blue-50"
+							>
+								Висока
+							</div>
+						</div>
+					</label>
+					<div class="hidden peer-checked:block">
+						<label
+							for="visoka"
+							class="inline-flex w-full p-2 bg-blue-50 rounded-md cursor-pointer text-lg font-semibold text-transparent bg-clip-text"
+							>Висока
+						</label>
+					</div>
+				</div>
+			</li>
+		</ul>
+	</div>
+</body>
 
-<div class="flex justify-center">
-<h1 class="text-8xl">Hydro homies</h1>
-</div>
+<style lang="postcss"></style>
