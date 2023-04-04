405d01d7f3a4e45290216e45480d9b7a6224927b
gg <>
2023-04-04 16:35:49 +0300

    update: Input weight

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,6 +21,22 @@
 			</li>
 		</ul>
 	</Box>
+	<Box>
+		<PromptTitle title="Тегло" />
+		<div class="bg-gradient-to-r from-cyan-500 to-blue-500 pl-4 rounded-lg p-1">
+			<div class="flex flex-row bg-blue-50 rounded-md">
+				<input
+					type="number"
+					min="0"
+					class="inline-flex w-full p-2 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-md cursor-pointer text-lg font-semibold text-transparent bg-clip-text focus:outline-none [-moz-appearance:_textfield] [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none caret-blue-500"
+				/>
+				<label
+					class="inline-flex p-2 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-md cursor-pointer text-lg font-semibold text-transparent bg-clip-text"
+					>кг</label
+				>
+			</div>
+		</div></Box
+	>
 </body>
 
 <style lang="postcss"></style>
