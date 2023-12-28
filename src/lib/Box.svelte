030de6d82e7e720598ae3e6f4d3ca4a346ee2343
G.Getsov <G.Getsov.Dev@gmail.com>
2023-12-28 02:33:52 +0200

    update: Shadows

--- src/lib/Box.svelte
+++ src/lib/Box.svelte
@@ -1,5 +1,5 @@
 <div class="px-2 py-4">
-	<div class="mx-auto max-w-xl rounded-lg border-4 border-blue-50 bg-blue-50 p-2">
+	<div class="mx-auto max-w-xl rounded-lg border-4 border-blue-50 bg-blue-50 p-2 shadow-lg">
 		<slot />
 	</div>
 </div>
--- src/lib/Title.svelte
+++ src/lib/Title.svelte
@@ -2,7 +2,9 @@
 	let title = 'Hydro homies';
 </script>
 
-<div class="justify-left mb-7 flex rounded-b-lg border-4 border-t-0 border-blue-50 bg-blue-50 p-2">
+<div
+	class="justify-left mb-7 flex rounded-b-lg border-4 border-t-0 border-blue-50 bg-blue-50 p-2 shadow-lg"
+>
 	<h1
 		class="bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-3xl font-extrabold text-transparent"
 	>
