fc02717ff2d35176f833d6594118f0d87f178b89
G.Getsov <G.Getsov.Dev@gmail.com>
2023-12-28 02:34:27 +0200

    update: Variable button color gradient

--- src/lib/ButtonHandler.svelte
+++ src/lib/ButtonHandler.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
 	export let label: string;
 	export let id: string;
+	export let gradient: string = 'from-cyan-500 to-blue-500';
+
+	$: nonPressedDivClass = `rounded-lg bg-gradient-to-r ${gradient} p-1 pl-4`;
+	$: pressedDivClass = `w-full bg-gradient-to-r ${gradient} bg-clip-text text-lg font-semibold text-transparent peer-checked/radio:bg-blue-50`;
 </script>
 
-<div class="rounded-lg bg-gradient-to-r from-cyan-500 to-blue-500 p-1 pl-4">
+<div class={nonPressedDivClass}>
 	<slot />
 	<label
 		for={id}
-		class="inline-flex w-full cursor-pointer rounded-md bg-blue-50 p-2 text-blue-500 peer-checked:hidden"
+		class="inline-flex w-full cursor-pointer rounded-md bg-blue-50 p-2 peer-checked:hidden"
 	>
 		<div class="block">
-			<div
-				class="w-full bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-lg font-semibold text-transparent peer-checked/radio:bg-blue-50"
-			>
+			<div class={pressedDivClass}>
 				{label}
 			</div>
 		</div>
